9750173970e3127e5d76b76e905102f3bd738aab
Clevente <122459102+Clevente@users.noreply.github.com>
2024-11-24 19:59:31 +0100

    Migrations and UserFactory

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,9 @@ return new class extends Migration {
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('admin')->default(false);
+            $table->string('phone_number');
+            $table->string('card_number');
             $table->rememberToken();
             $table->timestamps();
         });
