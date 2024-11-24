1e584d646aecfb9b7cbfba2f557c2733faf8692b
Clevente <122459102+Clevente@users.noreply.github.com>
2024-11-24 19:59:31 +0100

    Migrations and UserFactory

--- database/migrations/2024_11_24_194210_create_user_room_entries_table.php
+++ database/migrations/2024_11_24_194210_create_user_room_entries_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('user_room_entries', function (Blueprint $table) {
             $table->id();
+            $table->boolean('successful');
             $table->timestamps();
         });
     }
