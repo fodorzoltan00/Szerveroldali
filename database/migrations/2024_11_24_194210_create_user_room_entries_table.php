c96e51b8a8f543d6d5fef953fd5908f516e0c102
Fodor Zoltán <fodorzoltan00@gmail.com>
2024-11-24 20:41:51 +0100

    Add relationships and update migrations with foreign keys
    Introduced Eloquent relationships in Room, Position, User, and UserRoomEntry models to define associations. Updated migrations to include relevant foreign keys, enhancing data integrity and easing relationship management.

--- database/migrations/2024_11_24_194210_create_user_room_entries_table.php
+++ database/migrations/2024_11_24_194210_create_user_room_entries_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('user_room_entries', function (Blueprint $table) {
             $table->id();
             $table->boolean('successful');
+            $table->foreignId('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
