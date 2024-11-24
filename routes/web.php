ec857b3f266cb187333ec31a351afa38b17a3ecd
Fodor Zoltán <fodorzoltan00@gmail.com>
2024-11-24 23:19:17 +0100

    Add views and routes for workers, positions, and rooms
    Created new Blade templates for worker, position, and room pages. Added corresponding routes and controllers to manage these views. Integrated Bootstrap for styling and ensured a consistent layout across pages.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PositionController;
+use App\Http\Controllers\RoomController;
+use App\Http\Controllers\WorkerController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/room',[RoomController::class,'index'])->name('room');
+Route::get('/worker',[WorkerController::class,'index'])->name('worker');
+Route::get('/position',[PositionController::class,'index'])->name('position');
