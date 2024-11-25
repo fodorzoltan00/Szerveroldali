ce5753dcff0e63e3916b89a69a1d7aaa901d9f8f
Fodor Zoltán <fodorzoltan00@gmail.com>
2024-11-25 21:38:21 +0100

    Refactor views to use layout and enhance navbar.
    Updated `homepage.blade.php` to extend `app.blade.php` for consistency and maintainability. Added new authenticated links to the navbar in `app.blade.php` for better navigation.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,17 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-
+                        @auth
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Workers</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Positions</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Rooms</a>
+                        </li>
+                        @endauth
                     </ul>
 
                     <!-- Right Side Of Navbar -->
@@ -49,23 +59,44 @@
                                 </li>
                             @endif
                         @else
+
+
+
+
                             <li class="nav-item dropdown">
+
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+
                                     {{ Auth::user()->name }}
+
                                 </a>
 
+
+
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+
                                     <a class="dropdown-item" href="{{ route('logout') }}"
+
                                        onclick="event.preventDefault();
+
                                                      document.getElementById('logout-form').submit();">
+
                                         {{ __('Logout') }}
+
                                     </a>
 
+
+
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+
                                         @csrf
+
                                     </form>
+
                                 </div>
+
                             </li>
+
                         @endguest
                     </ul>
                 </div>
